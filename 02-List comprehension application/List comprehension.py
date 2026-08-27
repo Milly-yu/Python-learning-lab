335e93e5db06a1a7b393c759fc8be66e0812284b
Milly-yu <millyyu123@gmail.com>
2026-08-27 12:27:54 -0700

    Improve comment clarity in list comprehension

--- 02-List comprehension application/List comprehension.py
+++ 02-List comprehension application/List comprehension.py
@@ -1,3 +1,3 @@
 compri = [x['name'] for x in tester['info']]
-# make the sub-dictionary intermidiate variable, and the iterable is the lsit of sub-dictionaries
+# Make the sub-dictionary the intermidiate variable to connect the expression and the iterable.
 print(compri)
