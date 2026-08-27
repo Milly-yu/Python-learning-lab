f9df2972018e2382a83dedc532ddc018a33583c5
Milly-yu <millyyu123@gmail.com>
2026-08-27 12:09:36 -0700

    Add comment explaining list comprehension usage
    # make x the intermediate variable-- sub-dictionary, and the iterable is the list of sub-dictionaries

--- 02-List comprehension application/List comprehension.py
+++ 02-List comprehension application/List comprehension.py
@@ -1,2 +1,3 @@
-compri = [x['name'] for x in tester['info']]   
+compri = [x['name'] for x in tester['info']]
+# make x the intermidiate variable-- sub-dictionary, and the iterable is the lsit of sub-dictionaries
 print(compri)
