81b4660acce6848eb547c1b866ee41f5e8294107
Milly-yu <millyyu123@gmail.com>
2026-08-27 19:09:36 -0700

    Fix comment on data type inversion for max_interest
    Corrected comment to specify inverting data type instead of data.

--- 01-SP500-Analysis/SP500.py
+++ 01-SP500-Analysis/SP500.py
@@ -1,7 +1,7 @@
 fileref = open('SP500.txt', 'r')
 lines = fileref.readlines()
 sum_SP = 0
-max_interest = float(lines[6].split(',')[5]) # remeber to invert the data to float
+max_interest = float(lines[6].split(',')[5]) # remeber to invert the data type to float
 for line in lines[6:18]: # iterate from June 2016 through May 2017    
     sum_SP += float(line.split(',')[1])
     if float(line.split(',')[5]) > max_interest:
