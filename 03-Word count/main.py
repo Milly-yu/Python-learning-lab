7b382fccf450defe4f4aff877e8f8bf7145cf8be
Milly-yu <millyyu123@gmail.com>
2026-09-01 11:15:45 -0700

    Correctly assign max_value_key for word counts
    Fix assignment of max_value_key to ensure it holds the key with the maximum count.

--- 03-Word count/main.py
+++ 03-Word count/main.py
@@ -6,7 +6,7 @@ for word in sentence_list:
     word_counts[word] = word_counts.get(word, 0) + 1
 
 ks = list(word_counts.keys())
-ks[0] = max_value_key
+max_value_key = ks[0]
 # Assign the first key to max_value_key, then compare its value with the next key, and assign the one with a larger value to max_value_key. Repeat the process till the end.
 for k in ks:
     if word_counts[k] > word_counts[max_value_key]:
