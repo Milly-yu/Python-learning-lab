0e728dcf7a276f0f97f06bc804d1eb2494d5f9f7
Milly-yu <millyyu123@gmail.com>
2026-09-01 12:07:04 -0700

    Implement weather responses based on rain percentage
    Added a nested if-elif-else structure to respond to rain percentages.

---
+++ 05-Nested if-else/main.py
@@ -0,0 +1,14 @@
+percent_rain = [94.3, 45, 100, 78, 16, 5.3, 79, 86]
+
+resps = []
+for per in percent_rain:
+    if per > 90:
+        resps.append('Bring an umbrella.')
+    elif per > 80:
+        resps.append('Good for the flowers?')
+    elif per > 50:
+        resps.append('Watch out for clouds!')
+    else:
+        resps.append('Nice day!')
+print(resps)
+# reminder: nothing special, just pay attention to some details: 1)the format of the nested if-elif-else; 2)don't forget the period, question  mark, and exclamation mark after each string.
